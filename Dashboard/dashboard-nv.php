557dd3800e5af19c007540613588c993196cc88c
viluong6457 <87838153+viluong6457@users.noreply.github.com>
2022-12-15 04:00:24 +0700

    Add files via upload

---
+++ Dashboard/dashboard-nv.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="template.css">
+    <link rel="stylesheet" href="dashboard.css">
+    <title>UWC2.0</title>
+</head>
+
+<body>
+    <header>
+        <div class="inner-header">
+            <span class="menu">
+                <li><img src="./images/home.png" width="30px" class="icon"> <a href="../homepage/homepage.php">TRANG CHỦ
+                    </a>
+                </li>
+                <li><a href="./dashboard.php"> <b>DASHBOARD</b></a> </li>
+                <li><a href="#">THÔNG TIN</a> </li>
+                <li><a href="#">PHƯƠNG TIỆN</a> </li>
+                <li><a href="#">TUYẾN ĐƯỜNG</a> </li>
+                <li><a href="#">LỊCH LÀM VIỆC</a> </li>
+                <li><img src="./images/user.png" width="30px" class="icon"><a href="#">ĐĂNG XUẤT</a> </li>
+            </span>
+        </div>
+    </header>
+
+    <div class="title">TRANG CHỦ/ DASHBOARD</div>
+
+    <a href="#">
+        <span class="goto pic1">
+            <img src="./images/MCPs.png"><br>
+            <text>TUYẾN ĐƯỜNG</text>
+        </span>
+    </a>
+
+    <a href="#">
+        <span class="goto pic2">
+            <img src="./images/staff.png"><br>
+            <text>CẬP NHẬT THÔNG TIN</text>
+        </span>
+    </a>
+
+    <a href="#">
+        <span class="goto pic3">
+            <img src="./images/update.png"><br>
+            <text>CẬP NHẬT</text>
+        </span>
+    </a>
+
+    <a href="#">
+        <span class="goto pic4">
+            <img src="./images/vehicle.png"><br>
+            <text>PHƯƠNG TIÊN</text>
+        </span>
+    </a>
+
+    <a href="#">
+        <span class="goto pic5">
+            <img src="./images/task.png"><br>
+            <text>BÁO CÁO TIẾN ĐỘ</text>
+        </span>
+    </a>
+    <img src="./images/schedule.png" class="sched">
+    <span class="task-status">
+        <div class="head">HIỆN TRẠNG NHIỆM VỤ</div>
+        <div>
+            <span class="task">Nhiệm vụ 1: Xác nhận nhiệm vụ</span>
+            <span>Tình trạng: Hoàn thành</span>
+        </div>
+        <div>
+            <span class="task">Nhiệm vụ 2: Lấy phương tiện làm việc</span>
+            <span>Tình trạng: Hoàn thành</span>
+        </div>
+        <div>
+            <span class="task">Nhiệm vụ 3: Báo cáo hoàn thành</span>
+            <span>Tình trạng: Chưa hoàn thành</span>
+        </div>
+
+    </span>
+
+    <footer>
+        <span>Organization X copyright © by Group2_L01</span>
+    </footer>
+</body>
+
+</html>
